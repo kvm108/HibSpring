35d6c511be9124c6f36eb0c97ec999444d66f9a8
KV Maetreyi <maitreyi_kv@India.XoriantCorp.com>
2017-09-01 13:26:28 +0530

    Spring Hibernate

---
+++ SpringHibernateIntgr/src/main/java/com/spring/HibernateApp.java
@@ -0,0 +1,19 @@
+package com.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
+@ImportResource("beans.xml")
+public class HibernateApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateApp.class, args);
+	}
+
+}
---
+++ SpringHibernateIntgr/src/main/java/com/spring/dao/EmployeeHibernateDAOSupportImpl.java
@@ -0,0 +1,18 @@
+package com.spring.dao;
+
+import org.springframework.orm.hibernate5.support.HibernateDaoSupport;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.spring.entity.Employee;
+
+public class EmployeeHibernateDAOSupportImpl extends HibernateDaoSupport 
+	implements EmployeeDAO {
+
+	@Transactional
+	public void save(Employee employee) {
+		getHibernateTemplate().save(employee);
+	}
+	
+
+
+}
